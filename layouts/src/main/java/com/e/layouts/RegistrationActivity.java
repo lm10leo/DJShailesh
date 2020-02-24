0fc5a6c975db98e50f20413f0a4e0c587840c8ed
shantanu1997 <shantanuwankhade4999@gmail.com>
2020-02-24 13:25:59 +0530

    Validation

--- layouts/src/main/java/com/e/layouts/RegistrationActivity.java
+++ layouts/src/main/java/com/e/layouts/RegistrationActivity.java
@@ -3,9 +3,15 @@ package com.e.layouts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
+    Bundle bundle=null;
+    Button signUp;
+    EditText edtFirstName,edtMiddleName,edtLastName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,15 @@ public class RegistrationActivity extends AppCompatActivity {
         {
             Toast.makeText(this, ""+bundle.getString("callFrom"), Toast.LENGTH_LONG).show();
         }
+        signUp=findViewById(R.id.txtSignUp);
+        edtFirstName=findViewById(R.id.edtFirstName);
+        edtMiddleName=findViewById(R.id.edtMiddleName);
+        edtLastName=findViewById(R.id.edtLastName);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
